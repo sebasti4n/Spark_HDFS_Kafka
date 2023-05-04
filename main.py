614bdf21aa16615f23013bd5b70b1cf364388056
Mario Sebastian Prieto Macana <32579304+sebasti4n@users.noreply.github.com>
2023-05-04 18:14:47 -0500

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from pymodbus.client.sync import ModbusSerialClient
-
-client =  ModbusSerialClient(method='rtu', port="", baudrate, bytesize=8, stopbits=1, parity='N')
-connection = client.connect()
-print(connection)
-
-res = client.read_input_registers(address=0, count=10, unit=1)
-print(res.registers)
